9c2c100e130cdd250163b66753fea744af6e4d67
Joshua Torrance <joshuatorrance@gmail.com>
2012-08-22 13:13:39 +1000

    Big changes. Getting rid of the prebuild and adapting the homebuilt lit rev.

--- thesis.d
+++
@@ -1,1 +0,0 @@
-thesis.dvi thesis.d : thesis.tex Latex/Macros/MacroFile1.tex 0_frontmatter/abstract.tex 0_frontmatter/dedication.tex 0_frontmatter/acknowledgement.tex 0_frontmatter/glossary.tex 1_introduction/introduction.tex 2/aims.tex 3/XYZ.tex 4/XYZ.tex 5/XYZ.tex 6/XYZ.tex 7/discussion.tex 8/materials_methods.tex 9_backmatter/declaration.tex  9_backmatter/references.bib   
